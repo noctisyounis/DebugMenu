d0e7be159832d3c9c594d82eb5e6c50a655d9003
Nameyho <mathieuallard1993@hotmail.com>
2021-06-16 12:39:30 +0200

    DebugUiMenuManager

---
+++ DebugMenu/Assets/ui/Mathieu/Scripts/ButtonController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    #region exposed
+
+    #endregion
+    #region Methods
+
+
+    public void Onchoose()
+    {
+        
+    }
+    #endregion
+}
