a4bfc081c01ea6294b0c24b9e6f6782593ba44c3
Amaury Deltenre <amaury.deltenre@gmail.com>
2021-06-17 14:23:42 +0200

    Commit ExitDebugMenu

---
+++ DebugMenu/Assets/ui/Amaury/Scripts/DebugMenuVisibility.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMenuVisibility : MonoBehaviour
+{
+    #region Main
+
+    public void Hide()
+    {
+        Debug.Log("Hide");
+    }
+
+    public void Show()
+    {
+        Debug.Log("Show");
+    }
+
+    #endregion
+}
