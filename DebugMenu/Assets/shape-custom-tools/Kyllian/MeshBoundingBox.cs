c4652123a4159d5812bb81a32c58f41c935ac0c1
Breniak <kyllian_bocklandt@hotmail.com>
2021-06-18 14:54:22 +0200

    change ColliderBoundingBox to MeshBoundingBox

--- DebugMenu/Assets/shape-custom-tools/Kyllian/MeshBoundingBox.cs
+++ DebugMenu/Assets/shape-custom-tools/Kyllian/MeshBoundingBox.cs
@@ -3,23 +3,33 @@ using Shapes;
 using UnityEngine;
 using DebugAttribute;
 
-public class ColliderBoundingBox : MonoBehaviour
+public class MeshBoundingBox : MonoBehaviour
 {
     #region Unity API
 
     private void Start()
     {
-        _colliders = new List<Collider>();
-        _sphereColliders = new List<SphereCollider>();
+        _meshFilter = new List<MeshFilter>();
 
-        GetTypeOfColliders();
+        GetMeshInScene();
     }
 
     private void Update()
     {
         if (!_isDisplay) return;
 
-        DrawColliderBoundingBoxes();
+        DrawMeshBoundingBoxes();
+    }
+
+    private void OnGUI()
+    {
+        if(GUILayout.Button("Meshes"))
+        {
+            foreach (var item in _meshFilter)
+            {
+                Debug.Log(item);
+            }
+        }
     }
 
     #endregion
@@ -27,45 +37,24 @@ public class ColliderBoundingBox : MonoBehaviour
 
     #region Utils
 
-    [DebugMenu("Settings/Gizmos/BoundingBox")]
-    public static void SetColliderBoundingBox()
+    [DebugMenu("Settings/Gizmos/MeshBoundingBox")]
+    public static void SetMeshBoundingBox()
     {
         _isDisplay = !_isDisplay;
-        RefreshColliders();
+        RefreshMeshes();
     }
 
-    private static void RefreshColliders()
+    private static void RefreshMeshes()
     {
-        _colliders.Clear();
-        _sphereColliders.Clear();
-
-        GetTypeOfColliders();
+        _meshFilter.Clear();
+        GetMeshInScene();
     }
 
-    private static List<Collider> GetCollidersInScene()
+    private static void GetMeshInScene()
     {
-        List<Collider> collidersInScene = new List<Collider>();
-
-        foreach (var collider in FindObjectsOfTypeAll(typeof(Collider)) as Collider[])
+        foreach (var meshFilter in FindObjectsOfType(typeof(MeshFilter)) as MeshFilter[])
         {
-            collidersInScene.Add(collider);
-        }
-
-        return collidersInScene;
-    }
-
-    private static void GetTypeOfColliders()
-    {
-        foreach (var collider in GetCollidersInScene())
-        {
-            if (collider.GetType() == typeof(SphereCollider))
-            {
-                _sphereColliders.Add((SphereCollider)collider);
-            }
-            else
-            {
-                _colliders.Add(collider);
-            }
+            _meshFilter.Add(meshFilter);
         }
     }
 
@@ -74,39 +63,22 @@ public class ColliderBoundingBox : MonoBehaviour
 
     #region DrawingShape
 
-    private static void DrawColliderBoundingBoxes()
+    private static void DrawMeshBoundingBoxes()
     {
         Camera cam = Camera.main;
 
         using (Draw.Command(cam))
         {
-            Draw.Color = Color.red;
+            Draw.Color = Color.yellow;
 
-            foreach (var collider in _colliders)
+            foreach (var meshFilter in _meshFilter)
             {
-                DrawBoundingBox(collider);
-            }
-
-            foreach (var sphereCollider in _sphereColliders)
-            {
-                DrawBoundingSphere(sphereCollider);
+                DrawBoundingBox(meshFilter);
             }
         }
     }
 
-    private static void DrawBoundingSphere(SphereCollider collider)
-    {
-        Draw.RingThickness = 0.015f;
-        Vector3 center = collider.bounds.center;
-        float radius = collider.bounds.size.x * 0.5f;
-
-        Draw.DiscGeometry = DiscGeometry.Flat2D;
-        Draw.Ring(center, radius);
-        Draw.Ring(center, Quaternion.Euler(90, 0, 0), radius);
-        Draw.Ring(center, Quaternion.Euler(0, 90, 0), radius);
-    }
-
-    private static void DrawBoundingBox(Collider collider)
+    private static void DrawBoundingBox(MeshFilter meshFilter)
     {
         Draw.LineThickness = 0.02f;
         PolylinePath[] paths = new PolylinePath[6];
@@ -116,8 +88,8 @@ public class ColliderBoundingBox : MonoBehaviour
             paths[i] = new PolylinePath();
         }
 
-        Vector3 halfSize = collider.bounds.size * 0.5f;
-        Vector3 center = collider.bounds.center;
+        Vector3 halfSize = meshFilter.mesh.bounds.size * 0.5f;
+        Vector3 center = meshFilter.transform.position;
 
         Vector3 upFrontRightVertices = center + new Vector3(halfSize.x, halfSize.y, halfSize.z);
         Vector3 upFrontLeftVertices = center + new Vector3(-halfSize.x, halfSize.y, halfSize.z);
@@ -147,8 +119,7 @@ public class ColliderBoundingBox : MonoBehaviour
     #region private Members
 
     private static bool _isDisplay;
-    private static List<Collider> _colliders;
-    private static List<SphereCollider> _sphereColliders;
+    private static List<MeshFilter> _meshFilter;
 
     #endregion
 }
